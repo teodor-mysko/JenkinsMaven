9e78cd235ad68b19e3740b8bf25232cc1162c429
Teodor Mysko <teodor.mysko@gmail.com>
2015-09-08 21:39:19 +0300

    maven test for jenkins

---
+++ src/main/java/SomeMethods.java
@@ -0,0 +1,6 @@
+
+public class SomeMethods {
+	public int add (int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/AddTest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddTest {
+
+	private SomeMethods sm;
+
+	@Before
+	public void init() {
+		sm = new SomeMethods();
+	}
+
+	@Test
+	public void addTest() {
+		System.out.println("First test");
+		int k = sm.add(2, 10);
+		assertTrue("", k == 12);
+	}
+}
